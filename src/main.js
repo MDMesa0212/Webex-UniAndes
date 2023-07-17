a5ce5b84d53770897a5943630e77475d1d6dc8e3
MDMesa0212 <106718263+MDMesa0212@users.noreply.github.com>
2023-07-17 11:11:22 -0400

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,40 @@
+
+function setup() {
+
+  Alpine.store('model', {
+    currentPage: 'home', // 'home', 'service'
+    currentLanguage: 'english',
+    dialNumber: 'mdmesa@cisco.com',
+    services: [],
+
+    init() {
+      const params = new URLSearchParams(location.search);
+      if (params.has('number')) {
+        this.dialNumber = params.get('number');
+      }
+      this.services = [
+        { url: this.dialNumber, name: 'Llamar' },
+        { url: this.dialNumber, name: 'Apoyo' },
+        { url: this.dialNumber, name: 'Académico' },
+      ];
+    },
+    get page() {
+      return this.currentPage;
+    },
+    set page(nextPage) {
+      this.currentPage = nextPage;
+    },
+    currentLanguage: 'english',
+    languages: ['english', 'norwegian'],
+    get language() {
+      return this.currentLanguage;
+    },
+    set language(current) {
+      this.currentLanguage = current;
+    },
+  });
+
+}
+
+document.addEventListener('alpine:init', setup);
+
